f38a37ddd8ab0b7313890ad9a412af2275926228
Yann MARCOU <creativeyann17@gmail.com>
2023-12-09 14:59:50 -0500

    redirect https

--- api/src/main/java/com/creativeyann17/api/configurations/SecurityConfiguration.java
+++ api/src/main/java/com/creativeyann17/api/configurations/SecurityConfiguration.java
@@ -1,15 +1,24 @@
 package com.creativeyann17.api.configurations;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @ConfigurationProperties("security")
+@EnableWebSecurity
 @Data
+@Slf4j
 public class SecurityConfiguration {
 
   private boolean enabled;
@@ -17,5 +26,18 @@ public class SecurityConfiguration {
   private long rateLimit;
   private List<String> cors = new ArrayList<>();
   private List<String> publics = new ArrayList<>();
+
+  @Value("${server.ssl.key-store:}")
+  private String keyStore;
+
+  @Bean
+  public SecurityFilterChain filterChainWeb(HttpSecurity http) throws Exception {
+    http.cors().disable().csrf().disable().authorizeHttpRequests(req -> req.anyRequest().permitAll());
+    if (StringUtils.isNotBlank(keyStore)) {
+      log.info("Keystore found: {} ", keyStore);
+      http.requiresChannel().anyRequest().requiresSecure();
+    }
+    return http.build();
+  }
 
 }
